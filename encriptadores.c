4674b3861d3e510b5f0c5ca222fcdc657dec05a6
sol <msfontenla@fi.uba.ar>
2020-10-11 18:00:38 -0300

    Agrego el descifrado de cesar

--- encriptadores.c
+++ encriptadores.c
@@ -6,11 +6,18 @@
 
 
 int cifrado_de_cesar(char* cadena,int key){
-
   unsigned char* cadena_aux = (unsigned char*)cadena;
   while(*cadena_aux != '\0'){
     *cadena_aux = (unsigned char)(*cadena_aux + key) % 256;
+    cadena_aux++;
+  }
+  return EXITO;
+}
 
+int descifrado_de_cesar(char*cadena,int key){
+  unsigned char* cadena_aux = (unsigned char*)cadena;
+  while(*cadena_aux != '\0'){
+    *cadena_aux = (unsigned char)(*cadena_aux - key) % 256;
     cadena_aux++;
   }
   return EXITO;
--- encriptadores.h
+++ encriptadores.h
@@ -4,8 +4,8 @@
 /*precondiciones: la funcion recibe una cadena inicializada
 postcondiciones: la funcion encripta la cadena recibida por parametros por cifrado de cesar.
 devuelve EXITO en caso de encriptar correctamente y ERROR en caso de no poder*/
-int cifrado_de_cesar(char*cadena,int key);
-int descifrado_de_cesar(char*cadena,int key);
+int cifrado_de_cesar(char* cadena,int key);
+int descifrado_de_cesar(char* cadena,int key);
 
 
 #endif /*__ENCRIPTADORES_H__*/
