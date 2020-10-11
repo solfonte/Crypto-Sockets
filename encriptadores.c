85697737774c6215586aaeb3662acff58e84e92b
sol <msfontenla@fi.uba.ar>
2020-10-11 20:03:03 -0300

    agrego el de rc4 pero comentado por que falla

--- encriptadores.c
+++ encriptadores.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 #include "encriptadores.h"
 #include <stdlib.h>
+#define TAMANIO_VECTOR_S 255
 #define EXITO 1
 
-//hay mucho codigo repetido en los cifrados de cesar
+//hay mucho codigo repetido en los cifrados de cesar y vigenere
 int cifrado_de_cesar(char* cadena,int key){
   unsigned char* cadena_aux = (unsigned char*)cadena;
   while(*cadena_aux != '\0'){
-    *cadena_aux = (unsigned char)(*cadena_aux + key) % 256;
+    *cadena_aux = (unsigned char)(*cadena_aux + key) /*% 256*/;
     cadena_aux++;
   }
   return EXITO;
@@ -17,7 +18,7 @@ int cifrado_de_cesar(char* cadena,int key){
 int descifrado_de_cesar(char*cadena,int key){
   unsigned char* cadena_aux = (unsigned char*)cadena;
   while(*cadena_aux != '\0'){
-    *cadena_aux = (unsigned char)(*cadena_aux - key) % 256;
+    *cadena_aux = (unsigned char)(*cadena_aux - key) /*% 256*/;
     cadena_aux++;
   }
   return EXITO;
@@ -27,7 +28,7 @@ int cifrado_de_vigenere(char* cadena,char* key){
   unsigned char* cadena_aux = (unsigned char*)cadena;
   unsigned char* key_aux = (unsigned char*)key;
   while(*cadena_aux != '\0'){
-    *cadena_aux = (unsigned char)(*cadena_aux + *key_aux) % 256;
+    *cadena_aux = (unsigned char)(*cadena_aux + *key_aux)/* % 256*/;
     printf("|%i|",(int)*cadena_aux);
     cadena_aux++;
     key_aux++;
@@ -43,7 +44,7 @@ int descifrado_de_vigenere(char* cadena,char* key){
   unsigned char* cadena_aux = (unsigned char*)cadena;
   unsigned char* key_aux = (unsigned char*)key;
   while(*cadena_aux != '\0'){
-    *cadena_aux = (unsigned char)(*cadena_aux - *key_aux) % 256;
+    *cadena_aux = (unsigned char)(*cadena_aux - *key_aux)/* % 256*/;
     cadena_aux++;
     key_aux++;
     if(*key_aux == '\0'){
@@ -52,3 +53,49 @@ int descifrado_de_vigenere(char* cadena,char* key){
   }
   return EXITO;
 }
+/*
+static void swap(unsigned char *s, unsigned int i, unsigned int j) {
+    unsigned char temp = s[i];
+    s[i] = s[j];
+    s[j] = temp;
+}
+
+static void ksa(unsigned char *key, size_t key_length,unsigned char* vector_s) {
+    unsigned int i,j;
+    for (i = 0; i < 256; i++)
+        vector_s[i] = (unsigned char)i;
+
+    for (i = j = 0; i < 256; i++) {
+        j = (j + key[i % key_length] + vector_s[i]) & TAMANIO_VECTOR_S;
+        swap(vector_s, i, j);
+    }
+
+
+}
+
+static unsigned char rc4_output(unsigned char* vector_s) {
+  unsigned int i = 0,j = 0;
+
+    i = (i + 1) & TAMANIO_VECTOR_S;
+    j = (j + vector_s[i]) & TAMANIO_VECTOR_S;
+
+    swap(vector_s, i, j);
+
+    return vector_s[(vector_s[i] + vector_s[j]) & TAMANIO_VECTOR_S];
+}
+
+int cifrado_de_rc4(char* cadena,char* key){
+  unsigned char vector_s[TAMANIO_VECTOR_S];
+  unsigned char* cadena_aux = (unsigned char*)cadena;
+  ksa((unsigned char*)key,strlen(key),vector_s);
+  while(*cadena_aux != '\0'){
+    *cadena_aux = (unsigned char)(*cadena_aux ^ rc4_output(vector_s));
+    printf("|%i|",(int)*cadena_aux);
+
+    cadena_aux++;
+
+    }
+    return EXITO;
+
+}
+*/
--- encriptadores.h
+++ encriptadores.h
@@ -8,7 +8,7 @@ int cifrado_de_cesar(char* cadena,int key);
 int descifrado_de_cesar(char* cadena,int key);
 int cifrado_de_vigenere(char* cadena,char* key);
 int descifrado_de_vigenere(char* cadena,char* key);//seguro hay que castear a void las key en los sockets
-//int cifrado_de_rc4(char* cadena,char* key);
+int cifrado_de_rc4(char* cadena,char* key);
 //int descifrado_de_rc4(char* cadena,char* key);
 
 
