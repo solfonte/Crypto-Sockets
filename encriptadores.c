1f8608c0537af2b9dcf56fd5cf644af18fb9ab02
sol <msfontenla@fi.uba.ar>
2020-10-11 17:44:51 -0300

    ya and el cifrado de cesar

--- encriptadores.c
+++ encriptadores.c
@@ -1,21 +1,17 @@
 #include <string.h>
+#include <stdio.h>
+#include "encriptadores.h"
 #include <stdlib.h>
-#define N 3
+#define EXITO 1
 
-int encriptado_de_cesar(char*cadena){
-  //itero cadena
-  int longitud_cadena = strlen(cadena) - 1;
-  int contador = 0;
-  char* cadena_aux = malloc(longitud_cadena * sizeof(char*));
-  strncpy(cadena_aux,cadena,longitud_cadena);
-  while(contador <longitud_cadena){
 
-    char caracter = cadena_aux[contador];
-    unsigned int caracter_en_ascii_cifrado = (unsigned int)caracter + N;
-    caracter = (char)(caracter_en_ascii_cifrado % 256);
-    strcpy(cadena_aux[contador],&caracter);
+int cifrado_de_cesar(char* cadena,int key){
+
+  unsigned char* cadena_aux = (unsigned char*)cadena;
+  while(*cadena_aux != '\0'){
+    *cadena_aux = (unsigned char)(*cadena_aux + key) % 256;
+
+    cadena_aux++;
   }
-  free(cadena_aux);
-  return 1;
-  //al final a esa cadena nueva la copio a la cadena recibida por parametro
+  return EXITO;
 }
--- encriptadores.h
+++ encriptadores.h
@@ -4,6 +4,8 @@
 /*precondiciones: la funcion recibe una cadena inicializada
 postcondiciones: la funcion encripta la cadena recibida por parametros por cifrado de cesar.
 devuelve EXITO en caso de encriptar correctamente y ERROR en caso de no poder*/
-int encriptado_de_cesar(char*cadena);
+int cifrado_de_cesar(char*cadena,int key);
+int descifrado_de_cesar(char*cadena,int key);
 
-#endif __ENCRIPTADORES_H__
+
+#endif /*__ENCRIPTADORES_H__*/
