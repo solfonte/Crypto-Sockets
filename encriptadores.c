8f31b6a7f050413001952bc90b189ec6f24b2947
sol <msfontenla@fi.uba.ar>
2020-10-11 18:38:01 -0300

    agrego cifrado y descifrado de vigenere

--- encriptadores.c
+++ encriptadores.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #define EXITO 1
 
-
+//hay mucho codigo repetido en los cifrados de cesar
 int cifrado_de_cesar(char* cadena,int key){
   unsigned char* cadena_aux = (unsigned char*)cadena;
   while(*cadena_aux != '\0'){
@@ -19,6 +19,36 @@ int descifrado_de_cesar(char*cadena,int key){
   while(*cadena_aux != '\0'){
     *cadena_aux = (unsigned char)(*cadena_aux - key) % 256;
     cadena_aux++;
+  }
+  return EXITO;
+}
+
+int cifrado_de_vigenere(char* cadena,char* key){
+  unsigned char* cadena_aux = (unsigned char*)cadena;
+  unsigned char* key_aux = (unsigned char*)key;
+  while(*cadena_aux != '\0'){
+    *cadena_aux = (unsigned char)(*cadena_aux + *key_aux) % 256;
+    printf("|%i|",(int)*cadena_aux);
+    cadena_aux++;
+    key_aux++;
+    if(*key_aux == '\0'){
+      key_aux = (unsigned char*)key;
+    }
+  }
+  printf("\n");
+  return EXITO;
+}
+
+int descifrado_de_vigenere(char* cadena,char* key){
+  unsigned char* cadena_aux = (unsigned char*)cadena;
+  unsigned char* key_aux = (unsigned char*)key;
+  while(*cadena_aux != '\0'){
+    *cadena_aux = (unsigned char)(*cadena_aux - *key_aux) % 256;
+    cadena_aux++;
+    key_aux++;
+    if(*key_aux == '\0'){
+      key_aux = (unsigned char*)key;
+    }
   }
   return EXITO;
 }
--- encriptadores.h
+++ encriptadores.h
@@ -6,6 +6,10 @@ postcondiciones: la funcion encripta la cadena recibida por parametros por cifra
 devuelve EXITO en caso de encriptar correctamente y ERROR en caso de no poder*/
 int cifrado_de_cesar(char* cadena,int key);
 int descifrado_de_cesar(char* cadena,int key);
+int cifrado_de_vigenere(char* cadena,char* key);
+int descifrado_de_vigenere(char* cadena,char* key);//seguro hay que castear a void las key en los sockets
+//int cifrado_de_rc4(char* cadena,char* key);
+//int descifrado_de_rc4(char* cadena,char* key);
 
 
 #endif /*__ENCRIPTADORES_H__*/
