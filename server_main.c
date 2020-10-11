036e75df6121c6db474ab81246d512ef7cbc4697
sol <msfontenla@fi.uba.ar>
2020-10-11 20:49:20 -0300

    ya anda el parseo de los argumentos de server_main.c

--- server_main.c
+++ server_main.c
@@ -1,16 +1,22 @@
-#define CANTIDAD_ARGUMENTOS_SERVIDOR 4
+#define CANTIDAD_ARGUMENTOS 4
 #define SOBRAN_ARGUMENTOS "SOBRAN ARGUMENTOS"
 #define FALTAN_ARGUMENTOS "FALTAN ARGUMENTOS"
-#define POSICION_PUERTO 0
+#define POSICION_PUERTO 1
+#define POSICION_METODO 2
+#define POSICION_KEY 3
+#include <stdio.h>
+#include <stdlib.h>
 
-int int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[]) {
   if(argc != CANTIDAD_ARGUMENTOS){
     printf("ERROR: %s",argc < CANTIDAD_ARGUMENTOS? FALTAN_ARGUMENTOS:SOBRAN_ARGUMENTOS);
     return 0;
   }
-  int puerto = *(int*)argv[POSICION_PUERTO];
-  char* metodo = (char*)(argv[POSICION_METODO] + 9);
-  void* key = argv[POSICION_KEY] + 6;
-
+  int puerto = atoi(argv[POSICION_PUERTO]);
+  char const* metodo = argv[POSICION_METODO] + 9;
+  char const* key = argv[POSICION_KEY] + 6;
+  printf("puerto: %i\n",puerto);
+  printf("metodo: %s\n",metodo);
+  printf("key: %i\n",atoi(key));
   return 0;
 }
