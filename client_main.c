6688397a13992572d7ff96f7274afeb9fdee3f99
sol <msfontenla@fi.uba.ar>
2020-10-11 21:55:59 -0300

    agrego el client_main.c

--- client_main.c
+++ client_main.c
@@ -1,9 +1,12 @@
-#define CANTIDAD_ARGUMENTOS 4
+#define CANTIDAD_ARGUMENTOS 7
 #define SOBRAN_ARGUMENTOS "SOBRAN ARGUMENTOS"
 #define FALTAN_ARGUMENTOS "FALTAN ARGUMENTOS"
-#define POSICION_PUERTO 1
-#define POSICION_METODO 2
-#define POSICION_KEY 3
+#define POSICION_IP 1
+#define POSICION_PUERTO 2
+#define POSICION_METODO 3
+#define POSICION_KEY 4
+#define POSICION_ARCHIVO 6
+
 #include <stdio.h>
 #include <stdlib.h>
 /*
@@ -12,7 +15,7 @@ int main(int argc, char const *argv[]) {
     printf("ERROR: %s",argc < CANTIDAD_ARGUMENTOS? FALTAN_ARGUMENTOS:SOBRAN_ARGUMENTOS);
     return 0;
   }
-  int puerto = atoi(argv[POSICION_PUERTO]);
+  int puerto = atoi(argv[POSICION_IP]);
   char const* metodo = argv[POSICION_METODO] + 9;
   char const* key = argv[POSICION_KEY] + 6;
   printf("puerto: %i\n",puerto);
@@ -20,3 +23,4 @@ int main(int argc, char const *argv[]) {
   printf("key: %i\n",atoi(key));
   return 0;*/
 }
+//./client 127.0.0.1 8080 --method=rc4 --key=queso < __client_stdin__
